ac7ea33c3810ddee328cce32eb07d9a0eeaff243
ghus75 <44898433+ghus75@users.noreply.github.com>
2022-01-13 08:11:45 +0100

    add files via upload

---
+++ all_checks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+def check_reboot():
+    """ Returns True if computer is rebooting"""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print('pending reboot')
+        sys.exit(1)
+    print('everything ok')
+    sys.exit(0)
+
+main()
---
+++ free_memory.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python3
+
+def main():
+    """checks if enough memeory """
+
+    print('everything is ok')
+
+
+main()
