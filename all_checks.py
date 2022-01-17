6fc4a4cff0b2f59a36f26bda4fcde848729fa0f1
gaelhuser <gaelhuser@protonmail.com>
2022-01-17 02:38:09 +0100

    add disk_full fct, rename parameters

--- all_checks.py
+++ all_checks.py
@@ -6,10 +6,21 @@ def check_reboot():
     """ Returns True if computer is rebooting"""
     return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_gb, min_percent):
+    """returns True if not enough disk space"""
+    du = shutil.disk_usage(disk)
+    percent = 100 * du.free /du.total
+    gigabytes_free = du.freee/2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
 def main():
     if check_reboot():
-        """ checks for reboot pending """
         print('pending reboot')
+        sys.exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print('disk full')
         sys.exit(1)
     print('everything ok')
     sys.exit(0)
